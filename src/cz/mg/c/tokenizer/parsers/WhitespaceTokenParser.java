044440cb59e652f8697fcf0759075b27fe4ddd58
mg <gekoncze@seznam.cz>
2025-07-19 13:58:24 +0200

    simplified whitespace token parser

--- src/cz/mg/c/tokenizer/parsers/WhitespaceTokenParser.java
+++ src/cz/mg/c/tokenizer/parsers/WhitespaceTokenParser.java
@@ -3,10 +3,10 @@ package cz.mg.c.tokenizer.parsers;
 import cz.mg.annotations.classes.Service;
 import cz.mg.annotations.requirement.Mandatory;
 import cz.mg.annotations.requirement.Optional;
-import cz.mg.tokenizer.components.TokenParser;
 import cz.mg.token.tokens.WhitespaceToken;
 import cz.mg.tokenizer.components.CharacterReader;
 import cz.mg.tokenizer.components.TokenBuilder;
+import cz.mg.tokenizer.components.TokenParser;
 
 public @Service class WhitespaceTokenParser implements TokenParser {
     private static volatile @Service WhitespaceTokenParser instance;
@@ -28,23 +28,16 @@ public @Service class WhitespaceTokenParser implements TokenParser {
     @Override
     public @Optional WhitespaceToken parse(@Mandatory CharacterReader reader) {
         if (reader.has(this::space)) {
-            return parse(reader, new TokenBuilder(reader.getPosition()));
+            TokenBuilder builder = new TokenBuilder(reader.getPosition());
+            while (reader.has(this::space)) {
+                builder.append(reader.read());
+            }
+            return builder.build(WhitespaceToken::new);
         } else if (reader.has(this::tab) || reader.has(this::newline)) {
             return TokenBuilder.build(reader, WhitespaceToken::new);
         } else {
             return null;
         }
-    }
-
-    private @Mandatory WhitespaceToken parse(@Mandatory CharacterReader reader, @Mandatory TokenBuilder builder) {
-        while (reader.has()) {
-            if (reader.has(this::space)) {
-                builder.append(reader.read());
-            } else {
-                break;
-            }
-        }
-        return builder.build(WhitespaceToken::new);
     }
 
     private boolean space(char ch) {
